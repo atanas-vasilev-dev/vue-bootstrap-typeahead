c59444e13e287a5f69575fe637c8222f28e6d80e
Kaloyan Yosifov <kyosifov@2create.studio>
2019-10-08 15:57:41 +0300

    Add blur and focus events and support v-model empty field

--- src/components/VueBootstrapTypeahead.vue
+++ src/components/VueBootstrapTypeahead.vue
@@ -1,51 +1,55 @@
 <template>
-  <div>
-    <div :class="sizeClasses">
-      <div ref="prependDiv" v-if="$slots.prepend || prepend" class="input-group-prepend">
-        <slot name="prepend">
-          <span class="input-group-text">{{ prepend }}</span>
-        </slot>
-      </div>
-      <input
-        ref="input"
-        type="search"
-        :class="`form-control ${inputClass}`"
-        :placeholder="placeholder"
-        :aria-label="placeholder"
-        :value="inputValue"
-        @focus="isFocused = true"
-        @blur="handleBlur"
-        @input="handleInput($event.target.value)"
-        autocomplete="off"
-      />
-      <div v-if="$slots.append || append" class="input-group-append">
-        <slot name="append">
-          <span class="input-group-text">{{ append }}</span>
-        </slot>
-      </div>
-    </div>
-    <vue-bootstrap-typeahead-list
-      class="vbt-autcomplete-list"
-      ref="list"
-      v-show="isFocused && data.length > 0"
-      :query="inputValue"
-      :data="formattedData"
-      :background-variant="backgroundVariant"
-      :text-variant="textVariant"
-      :maxMatches="maxMatches"
-      :minMatchingChars="minMatchingChars"
-      @hit="handleHit"
-    >
-      <!-- pass down all scoped slots -->
-      <template v-for="(slot, slotName) in $scopedSlots" :slot="slotName" slot-scope="{ data, htmlText }">
-        <slot :name="slotName" v-bind="{ data, htmlText }"></slot>
-      </template>
-      <!-- below is the right solution, however if the user does not provide a scoped slot, vue will still set $scopedSlots.suggestion to a blank scope
+    <div>
+        <div :class="sizeClasses">
+            <div ref="prependDiv" v-if="$slots.prepend || prepend" class="input-group-prepend">
+                <slot name="prepend">
+                    <span class="input-group-text">{{ prepend }}</span>
+                </slot>
+            </div>
+            <input
+                ref="input"
+                type="search"
+                :class="`form-control ${inputClass}`"
+                :placeholder="placeholder"
+                :aria-label="placeholder"
+                :value="inputValue"
+                @focus="handleFocused"
+                @blur="handleBlur"
+                @input="handleInput($event.target.value)"
+                autocomplete="off"
+            />
+            <div v-if="$slots.append || append" class="input-group-append">
+                <slot name="append">
+                    <span class="input-group-text">{{ append }}</span>
+                </slot>
+            </div>
+        </div>
+        <vue-bootstrap-typeahead-list
+            class="vbt-autcomplete-list"
+            ref="list"
+            v-show="isFocused && data.length > 0"
+            :query="inputValue"
+            :data="formattedData"
+            :background-variant="backgroundVariant"
+            :text-variant="textVariant"
+            :maxMatches="maxMatches"
+            :minMatchingChars="minMatchingChars"
+            @hit="handleHit"
+        >
+            <!-- pass down all scoped slots -->
+            <template
+                v-for="(slot, slotName) in $scopedSlots"
+                :slot="slotName"
+                slot-scope="{ data, htmlText }"
+            >
+                <slot :name="slotName" v-bind="{ data, htmlText }"></slot>
+            </template>
+            <!-- below is the right solution, however if the user does not provide a scoped slot, vue will still set $scopedSlots.suggestion to a blank scope
       <template v-if="$scopedSlots.suggestion" slot="suggestion" slot-scope="{ data, htmlText }">
         <slot name="suggestion" v-bind="{ data, htmlText }" />
-      </template>-->
-    </vue-bootstrap-typeahead-list>
-  </div>
+            </template>-->
+        </vue-bootstrap-typeahead-list>
+    </div>
 </template>
 
 <script>
@@ -113,6 +117,13 @@ export default {
           text: this.serializer(d)
         }
       })
+    },
+
+    inputValue() {
+      if (typeof this.value !== 'undefined') {
+        return this.value
+      }
+      return this.noModelValue
     }
   },
 
@@ -137,10 +148,15 @@ export default {
         this.$emit('input', evt.text)
       }
 
-      this.inputValue = evt.text
+      this.noModelValue = evt.text
       this.$emit('hit', evt.data)
       this.$refs.input.blur()
       this.isFocused = false
+    },
+
+    handleFocused() {
+      this.$emit('focus')
+      this.isFocused = true
     },
 
     handleBlur(evt) {
@@ -148,11 +164,13 @@ export default {
       if (tgt && tgt.classList.contains('vbst-item')) {
         return
       }
+
+      this.$emit('blur')
       this.isFocused = false
     },
 
     handleInput(newValue) {
-      this.inputValue = newValue
+      this.noModelValue = newValue
 
       // If v-model is being used, emit an input event
       if (typeof this.value !== 'undefined') {
@@ -164,7 +182,7 @@ export default {
   data() {
     return {
       isFocused: false,
-      inputValue: ''
+      noModelValue: ''
     }
   },
 
@@ -183,11 +201,11 @@ export default {
 </script>
 
 <style scoped>
-  .vbt-autcomplete-list {
+.vbt-autcomplete-list {
     padding-top: 5px;
     position: absolute;
     max-height: 350px;
     overflow-y: auto;
     z-index: 999;
-  }
+}
 </style>
